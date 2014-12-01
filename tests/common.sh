028ff64995aa865881a44e3bcd8f26415f64e355
Tim Waugh <twaugh@redhat.com>
2014-12-01 16:12:41 +0000

    Make the sample PDF a valid one.

--- tests/common.sh
+++ tests/common.sh
@@ -99,12 +99,37 @@ sample_pdf () {
 %PDF-1.3
 %cupsRotation: 0
 3 0 obj
+<</Length 4 0 R
+>>
+stream
+q
+0 g
+BT
+  18 745.884 Td
+  105.882 Tz
+  /FN00 11.333 Tf <74657374> Tj
+ET
+Q
+endstream
+endobj
+4 0 obj
+75
+endobj
+5 0 obj
+<</Type/Page
+  /Parent 1 0 R
+  /MediaBox [0 0 612 792]
+  /Contents 3 0 R
+  /Resources << /Font 2 0 R >>
+>>
+endobj
+6 0 obj
 <</Type/Font
   /Subtype /Type1
   /BaseFont /Courier-Bold
 >>
 endobj
-4 0 obj
+7 0 obj
 <</Type/Font
   /Subtype /Type1
   /BaseFont /Courier
@@ -112,46 +137,49 @@ endobj
 endobj
 2 0 obj
 <<
-  /FB00 3 0 R
-  /FN00 4 0 R
+  /FB00 6 0 R
+  /FN00 7 0 R
 >>
 endobj
 1 0 obj
 <</Type/Pages
-  /Count 0
-  /Kids []
+  /Count 1
+  /Kids [5 0 R ]
 >>
 endobj
-5 0 obj
+8 0 obj
 <</Type/Catalog
   /Pages 1 0 R
 >>
 endobj
-6 0 obj
+9 0 obj
 <<
   /Creator (texttopdf/1.0.58)
-  /CreationDate (D:20141121162117+00'00')
+  /CreationDate (D:20141130012337+00'00')
   /Title ()
   /Author (test)
 >>
 endobj
 xref
-0 7
+0 10
 0000000000 65535 f 
-0000000220 00000 n 
-0000000171 00000 n 
+0000000486 00000 n 
+0000000437 00000 n 
 0000000026 00000 n 
-0000000101 00000 n 
-0000000274 00000 n 
-0000000323 00000 n 
+0000000152 00000 n 
+0000000170 00000 n 
+0000000292 00000 n 
+0000000367 00000 n 
+0000000546 00000 n 
+0000000595 00000 n 
 trailer
 <<
-  /Size 7
-  /Root 5 0 R
-  /Info 6 0 R
+  /Size 10
+  /Root 8 0 R
+  /Info 9 0 R
 >>
 startxref
-445
+717
 %%EOF
 EOF
     printf "%s" "$tmp"
